1f7074a4704547b8697af60cfbcd51dc8fe8ac0e
MadhuAdiga <madhu.vah@gmail.com>
2017-01-04 09:31:53 +0530

    Assignment3

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var sum=0; // Assign sum to zero
+for(i=0;i<=1000;i++){ // Loop i from 0 to 1000 with an increment of 1
+	sum=sum+i; // Add value of i to sum
+}
+console.log("The sum calculated through 'for loop' is " + sum);
+
+var i=0;
+var sum=0;   //Assign sum to zero
+while(i<=1000){ // Loop i from 0 to 1000
+sum=sum+i;	// Assign sum to zero
+i++;		// Increment i by 1
+}
+console.log("The sum calculated through 'while loop' is " + sum);	
